3f079a9a3242d9cce41bfd7145a835728a8ad43e
gmniprog <marius.porteboeuf@epfl.ch>
2022-04-28 11:36:39 +0200

    Working_distance

--- main.c
+++ main.c
@@ -18,11 +18,11 @@
 #include <sensors/VL53L0X/VL53L0X.h>
 
 //uncomment to send the FFTs results from the real microphones
-#define SEND_FROM_MIC
+//#define SEND_FROM_MIC
 
 //uncomment to use double buffering to send the FFT to the computer
 //#define DOUBLE_BUFFERING
-
+#define VL53L0X
 //salut Ludo
 
 static void serial_start(void)
@@ -88,13 +88,11 @@ int main(void)
         uint16_t distance = 0;
         distance = VL53L0X_get_dist_mm();
         if (distance < 55){
-        	left_motor_set_pos(0);
-        	right_motor_set_pos(0);
         	uint16_t nombrePourFaireUnTour = 2000;
         	uint16_t valActuelle = 0;
         	while (nombrePourFaireUnTour >= valActuelle){
         	right_motor_set_speed(-100);
-        	valActuelle=right_motor_get_pos();
+        	valActuelle+=1;
         	chprintf((BaseSequentialStream*)&SD3, "Hey! position = %d\n", valActuelle);
         	}
 
@@ -111,80 +109,8 @@ int main(void)
         //we copy the buffer to avoid conflicts
         arm_copy_f32(get_audio_buffer_ptr(LEFT_OUTPUT), send_tab, FFT_SIZE);
        SendFloatToComputer((BaseSequentialStream *) &SD3, send_tab, FFT_SIZE);
-#else
        //SendFloatToComputer((BaseSequentialStream *) &SD3, get_audio_buffer_ptr(LEFT_OUTPUT), FFT_SIZE);
 #endif  /* DOUBLE_BUFFERING */
-#else
-        //time measurement variables
-        volatile uint16_t time_fft = 0;
-        volatile uint16_t time_mag  = 0;
-
-        float* bufferCmplxInput = get_audio_buffer_ptr(LEFT_CMPLX_INPUT);
-        float* bufferOutput = get_audio_buffer_ptr(LEFT_OUTPUT);
-
-        //uint16_t size = ReceiveInt16FromComputer((BaseSequentialStream *) &SD3, bufferCmplxInput, FFT_SIZE);
-
-        if(size == FFT_SIZE){
-            /*
-            *   Optimized FFT
-            */
-            
-            chSysLock();
-            //reset the timer counter
-            GPTD12.tim->CNT = 0;
-
-            doFFT_optimized(FFT_SIZE, bufferCmplxInput);
-
-            time_fft = GPTD12.tim->CNT;
-            chSysUnlock();
-
-            /*
-            *   End of optimized FFT
-            */
-
-            /*
-            *   Non optimized FFT
-            */
-
-            // //need to convert the float buffer into complex_float struct array
-            // for(uint16_t i = 0 ; i < (2*FFT_SIZE) ; i+=2){
-            //     temp_tab[i/2].real = bufferCmplxInput[i];
-            //     temp_tab[i/2].imag = bufferCmplxInput[i+1];
-            // }
-
-            // chSysLock();
-            // //reset the timer counter
-            // GPTD12.tim->CNT = 0;
-
-            // //do a non optimized FFT
-            // doFFT_c(FFT_SIZE, temp_tab);
-
-            // time_fft = GPTD12.tim->CNT;
-            // chSysUnlock();
-            
-            // //reconverts the result into a float buffer
-            // for(uint16_t i = 0 ; i < (2*FFT_SIZE) ; i+=2){
-            //     bufferCmplxInput[i] = temp_tab[i/2].real;
-            //     bufferCmplxInput[i+1] = temp_tab[i/2].imag;
-            // }
-
-            /*
-            *   End of non optimized FFT
-            */
-
-            chSysLock();
-            //reset the timer counter
-            GPTD12.tim->CNT = 0;
-
-            arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
-
-            time_mag = GPTD12.tim->CNT;
-            chSysUnlock();
-
-            SendFloatToComputer((BaseSequentialStream *) &SD3, bufferOutput, FFT_SIZE);
-            //chprintf((BaseSequentialStream *) &SDU1, "time fft = %d us, time magnitude = %d us\n",time_fft, time_mag);
-
-        }
 #endif  /* SEND_FROM_MIC */
     }
 }
